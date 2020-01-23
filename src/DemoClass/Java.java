e4419abfbf2f11d608d7f5696dc4d5d4ad0f2a85
oscarcorona <ocorona@student.bridgew.edu>
2020-01-23 12:17:30 -0500

    demo

--- src/DemoClass/Java.java
+++ src/DemoClass/Java.java
@@ -1,1 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+
+public class Java{
+    public static void main(String [] args){
+        var reader= new Scanner (System.in);
+        System.out
+        }
+        }
