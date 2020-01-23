5a705d99ebf48a6d470f8812e1d6fb4a983cb715
oscarcorona <ocorona@student.bridgew.edu>
2020-01-23 11:49:59 -0500

    First Code ran correctly

--- src/Main/Java.java
+++ src/Main/Java.java
@@ -1,4 +1,7 @@
-package Main;
-
 public class Java {
+    public static void main(String[] args) {
+        System.out.println("Hello Comp152");
+        System.out.println("Let's make it work");
+
+    }
 }
